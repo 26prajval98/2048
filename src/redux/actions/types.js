11e1c1983fe5f6e8e812ad2fab7c063f928a3f8b
26prajval98 <26prajval98@gmail.com>
2018-07-21 00:52:09 +0530

    Done

--- src/redux/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const GENERATE_GRID = "GENERATE_GRID"
-export const UP_DOWN = "UP_DOWN"
-export const LEFT_RIGHT = "LEFT_RIGHT"
--- src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import generateGrid from './generateGrid'
-
-export default combineReducers({
-    generate : generateGrid
-})
